131ed4471afb44fc6fcfa8a6ce437a20931a9200
Nikita Polyakov <164608639+zerocandie@users.noreply.github.com>
2025-12-17 13:02:30 +0700

    Update index.js

--- 3laba/components/accordion/index.js
+++ 3laba/components/accordion/index.js
@@ -1,37 +1,25 @@
+// components/accordion/index.js
+
 export class AccordionComponent {
     constructor(parent, items, onItemClick) {
         this.parent = parent;
         this.items = items;
         this.onItemClick = onItemClick;
     }
-    getHTML(data) {
-        return `
-            <div class="card mb-3" style="width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="${data.photo_400_orig}" class="img-fluid" alt="картинка">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">${data.first_name} ${data.last_name}</h5>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `;
-    }
 
-    // ✅ Добавьте этот метод!
     render() {
-        // Генерируем HTML для всего аккордиона
-        const accordionHTML = this.items.map(item => `
+        const itemsHTML = this.items.map(item => `
             <div class="accordion-item">
                 <h2 class="accordion-header" id="heading-${item.id}">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-${item.id}" aria-expanded="false" aria-controls="collapse-${item.id}">
+                    <button class="accordion-button collapsed" type="button" 
+                        data-bs-toggle="collapse" 
+                        data-bs-target="#collapse-${item.id}" 
+                        aria-expanded="false" 
+                        aria-controls="collapse-${item.id}">
                         ${item.title}
                     </button>
                 </h2>
-                <div id="collapse-${item.id}" class="accordion-collapse collapse" aria-labelledby="heading-${item.id}" data-bs-parent="#accordion">
+                <div id="collapse-${item.id}" class="accordion-collapse collapse" aria-labelledby="heading-${item.id}">
                     <div class="accordion-body">
                         ${item.content}
                     </div>
@@ -39,19 +27,19 @@ export class AccordionComponent {
             </div>
         `).join('');
 
-        // Оборачиваем в контейнер аккордиона
+        // Вставляем в родительский контейнер
         this.parent.innerHTML = `
-            <div class="accordion" id="accordion">
-                ${accordionHTML}
+            <div class="accordion" id="dogAccordion">
+                ${itemsHTML}
             </div>
         `;
 
-        // Добавляем обработчики на кнопки "Подробнее"
-        this.parent.querySelectorAll('.details-btn').forEach(button => {
-            button.addEventListener('click', (e) => {
-                const dogId = e.target.dataset.id;
+        // Навешиваем обработчики на кнопки "Подробнее"
+        this.parent.querySelectorAll('.details-btn').forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                const id = e.target.dataset.id;
                 if (this.onItemClick) {
-                    this.onItemClick(dogId);
+                    this.onItemClick(id);
                 }
             });
         });
